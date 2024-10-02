fb2ca6346a4d21bf49b70c77153298f1173ece59
Kuun <kunnet882@gmail.com>
2024-10-02 19:27:55 +0700

    commit2222222

--- api.js
+++ api.js
@@ -1,13 +1,3 @@
-import express from 'express';
 import { handler } from './bin/www';
 
-const app = express();
-
-app.use(handler);
-
-export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: 'Kuun API',
-  };
-}
+export { handler };
